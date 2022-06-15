c7a3e5994d9738fe878438ef6fc0d2315e5fa7fc
shahzad.iqbal <shahzad.iqbal@bookassist.com>
2022-06-15 19:06:31 +0500

    java code for database

---
+++ src/main/java/JdbcTest.java
@@ -0,0 +1,19 @@
+import ui.ViewStudent;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JdbcTest {
+
+    public static void main(String[] args) {
+
+         new ViewStudent();
+        System.out.println("Hello world 1");
+        System.out.println("Hello world 2");
+        System.out.println("Hello world 3");
+        System.out.println("Hello world 4");
+
+    }
+}
